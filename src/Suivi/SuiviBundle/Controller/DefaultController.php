43a25320109bd17a7efca05a32449ed724b44285
ibradev <feki.abir@gmail.com>
2015-04-14 10:58:00 +0100

    DashboardCommercial_v1.4

--- src/Suivi/SuiviBundle/Controller/DefaultController.php
+++ src/Suivi/SuiviBundle/Controller/DefaultController.php
@@ -6,7 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-  
+    public function indexAction()
+    {
+        return $this->render('SuiviVenteBundle:Default:index.html.twig');
+    }
+
     public function adminAction()
     {
         return $this->render('SuiviVenteBundle:Default:admin.html.twig');
